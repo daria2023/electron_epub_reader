2c66294e3eedbac4b25de5f28271ef44f5adba71
zs <ishanzhang99@163.com>
2023-11-28 16:10:53 +0800

    style: style adjusted

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 const { app, BrowserWindow } = require('electron')
 const { run } = require("./server")
 
-const createWindow = () => {
+const createWindow = async () => {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
         autoHideMenuBar: true,
-        darkTheme: true
+        darkTheme: true,
+        title:`epub-reader`,
+        icon:"/favicon.ico",
     })
-    win.loadURL('http://localhost:9999');
+    await win.loadURL('http://localhost:9999');
 }
 
 app.whenReady().then(async () => {
     await run();
-    createWindow()
+    await createWindow()
 })
