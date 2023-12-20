3fd1a0aeb214d6c5034493e18d37ccb440966c85
zs <ishanzhang99@163.com>
2023-12-20 17:48:57 +0800

    fix:hide unfinished not list

--- README.md
+++ README.md
@@ -5,8 +5,11 @@ There are still something else i'm fixing:
 - the fetched images should be cleared after the chapter has read;
 - ~~more fonts and style can be provided~~(enough for me for chinese fonts);
 - ~~more theme can be provided, I set it to dark only because it fits my general pc theme~~(support light and dark themes now!).
-
+- Open note-list so that the user can save the parts they like locally!
+- release the first version exe package ;
 待完成：
 - ~~可能存在的端口占用问题~~；
 - 本章阅读完后清理加载的图片；
 - ~~更多字体、样式和主题提供~~；
+- 开启保存笔记功能；
+- 发布第一版Windows exe安装包；
--- static/index.js
+++ static/index.js
@@ -1,81 +1,87 @@
-const file_list  = document.getElementById('file-list');
-const note_list = document.getElementById('notes-list');
-const fileInput = document.getElementById('uploading');
+const file_list = document.getElementById("file-list");
+// const note_list = document.getElementById('notes-list');
+const fileInput = document.getElementById("uploading");
 
+const fetchData = (cfg, empty, container, cb) => {
+  fetch(cfg.url, {
+    method: cfg.method,
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((res) => {
+      if (res.code === 200 && res.data.length > 0) {
+        cb(res.data);
+      } else {
+        const pEl = document.createElement("p");
+        pEl.innerHTML = empty;
+        container.appendChild(pEl);
+      }
+    });
+};
 
-const fetchData = (cfg, empty, container,cb) => {
-    fetch(cfg.url,{
-        method: cfg.method
-    }).then(res => {
-        return (res.json());
-    }).then(res => {
-        if(res.code === 200 && res.data.length > 0) {
-            cb(res.data)
-        } else {
-            const pEl = document.createElement('p');
-            pEl.innerHTML = empty;
-            container.appendChild(pEl);
+fetchData(
+  { url: "/books", method: "GET" },
+  "No books yet, add some!",
+  file_list,
+  (data) => {
+    data.map((li) => {
+      const l = document.createElement("li");
+      const a = document.createElement("a");
+      a.innerHTML = li;
+      l.append(a);
+      file_list.appendChild(l);
+      l.addEventListener("click", async () => {
+        const encodedFilename = encodeURIComponent(li);
+        const res = await fetch("/read/" + encodedFilename);
+        const data = await res.json();
+        if (data.code === 200) {
+          window.location.replace(
+            "http://localhost:9877/info/" + encodedFilename,
+          );
         }
-    })
-}
+      });
+    });
+  },
+);
 
-fetchData({url:'/books', method:'GET'}, 'No books yet, add some!', file_list, (data)=>{
-    data.map(li => {
-        const l = document.createElement('li');
-        const a = document.createElement('a');
-        a.innerHTML = li;
-        l.append(a);
-        file_list.appendChild(l);
-        l.addEventListener('click', async () => {
-            const encodedFilename = encodeURIComponent(li);
-            const res = await fetch('/read/'+ encodedFilename);
-            const data = await res.json()
-                if(data.code === 200) {
-                    window.location.replace('http://localhost:9877/info/'+encodedFilename);
-                }
-        })
-    })
+// fetchData({url:'/notes', method:'GET'}, 'No books yet, add some!', note_list, (data)=>{
+//     data.map(li => {
+//         const l = document.createElement('li');
+//         const a = document.createElement('a');
+//         a.innerHTML = li;
+//         l.append(a);
+//         note_list.appendChild(l);
+//         l.addEventListener('click', async () => {
+//             const encodedFilename = encodeURIComponent(li);
+//             console.log('1121')
+//
+//             const res = await fetch('/read/'+ encodedFilename);
+//             console.log('1121')
+//             const data = await res.json();
+//             console.log(data, await res)
+//                 if(data.code === 200) {
+//                     console.log('okk')
+//                     window.location.replace(`http://localhost:9877/info/${li}`);
+//                 }
+//             })
+//     })
+// })
+
+fileInput.addEventListener("change", async (e) => {
+  const file = e.target.files[0];
+  await handleFile(file);
 });
 
-fetchData({url:'/notes', method:'GET'}, 'No books yet, add some!', note_list, (data)=>{
-    data.map(li => {
-        const l = document.createElement('li');
-        const a = document.createElement('a');
-        a.innerHTML = li;
-        l.append(a);
-        note_list.appendChild(l);
-        l.addEventListener('click', async () => {
-            const encodedFilename = encodeURIComponent(li);
-            console.log('1121')
-
-            const res = await fetch('/read/'+ encodedFilename);
-            console.log('1121')
-            const data = await res.json();
-            console.log(data, await res)
-                if(data.code === 200) {
-                    console.log('okk')
-                    window.location.replace(`http://localhost:9877/info/${li}`);
-                }
-            })
-    })
-})
-
-
-fileInput.addEventListener('change', async e =>{
-    const file = e.target.files[0];
-    await handleFile(file)
-})
-
-const handleFile = async file => {
-    const formData = new FormData();
-    formData.append('file', file)
-    const res = await fetch('/',{
-        method:'post',
-        body: formData
-    });
-    const status = await res.status
-    if(status === 200){
-        window.location.reload();
-    }
-
-}
+const handleFile = async (file) => {
+  const formData = new FormData();
+  formData.append("file", file);
+  const res = await fetch("/", {
+    method: "post",
+    body: formData,
+  });
+  const status = await res.status;
+  if (status === 200) {
+    window.location.reload();
+  }
+};
